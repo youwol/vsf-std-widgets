6e1f4cde92b2a65e98d61d1f97a969ae48832d33
youwol-guillaume <greinisch@youwol.com>
2023-06-19 09:20:27 +0200

    📝 add minimal global documentation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+/**
+ * Visual Studio Flow toolbox exposing standard HTML widgets.
+ *
+ * For comprehensive information about the Visual Studio Flow (vs-flow) environment as a whole, a user guide can be
+ * accessed <a href="https://l.youwol.com/doc/@youwol/vs-flow" target="_blank" >here</a>.
+ * For developers seeking detailed documentation, please refer to the resources available
+ *  <a href="https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/vs-flow&tab=doc" target="_blank" >here</a>.
+ *
+ * We encourage you to participate in the development process of this toolbox by opening issues or contributing to the
+ * project to add new modules and features. Your feedback and contributions are highly appreciated.
+ *
+ * @module
+ */
 export * from './lib'
-export {setup} from './auto-generated'
+export { setup } from './auto-generated'
